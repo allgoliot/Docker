e42ef0f5f548b3ad1bbc83f62dc3cca4ec36ebc3
Allan Goliot <allan.goliot@gmail.com>
2019-03-05 19:48:19 +0100

    Add install docker-compose

--- install.sh
+++ install.sh
@@ -17,6 +17,13 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname 
+-s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
