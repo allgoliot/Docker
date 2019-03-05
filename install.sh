409d650e5e66ba597a0c849349ee905abf60acc9
Allan Goliot <allan.goliot@gmail.com>
2019-03-05 22:08:15 +0100

    Add install docker-compose

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 
 #! /bin/bash
+
+echo "*** Removing docker ***"
+
 sudo yum remove docker \
                   docker-client \
                   docker-client-latest \
@@ -16,9 +19,14 @@ sudo yum install -y yum-utils \
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-
+echo "*** Installing docker ***"
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
+echo "Docker installed "
+docker --version
+echo ""
+
+echo "*** Installing docker-compose ***"
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname 
 -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -27,3 +35,6 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+echo "Docker-compose installed"
+docker-compose --version
