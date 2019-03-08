4d914c7d8a9bedd5fe4fa53390181e88343d0d3a
root <root@instance-3.us-east1-b.c.golden-furnace-233615.internal>
2019-03-08 09:19:52 +0000

    modification run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
-./install.sh
+if [ -f /bin/docker ] 
+then
+	echo "Docker & Docker-compose déjà installé"
+	echo "Lancement du site"
+else
+	./install.sh	
+fi
 
 cd wordpress/with_mysql/
+systemctl start docker 
 docker-compose up
